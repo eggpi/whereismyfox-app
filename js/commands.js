c8b09e4f098dce4362ad61e6bafba22f352bc6d5
Guilherme Gonçalves <guilherme.p.gonc@gmail.com>
2013-10-25 11:41:21 -0200

    Accept arguments to wipe all options for DeviceStorage.

--- js/commands.js
+++ js/commands.js
@@ -39,12 +39,42 @@ const commands = [
 
   {
     id: 2,
-    command: function command_wipe(me) {
-      if (navigator.mozPower && navigator.mozPower.factoryReset) {
-        navigator.mozPower.factoryReset();
-      } else {
-        // FIXME can this really happen?
-        console.error("Something's wrong, mozPower is not available!");
+    command: function command_wipe(me, args) {
+      function wipeDeviceStorageTarget(target) {
+        function exhaustCursorAndWipe() {
+          var file = this.result;
+          if (file !== null) {
+            var remove = storage.delete(file.name);
+            remove.onerror = function() {
+              console.error("failed to remove '" + file.name + "'! " + this.error);
+            }
+          }
+
+          if (!this.done) {
+            this.continue();
+          }
+        }
+
+        var storage = navigator.getDeviceStorage(target);
+        var cursor = storage.enumerate();
+        cursor.onsuccess = exhaustCursorAndWipe;
+      }
+
+      var dstargets = ["pictures", "videos", "sdcard", "music", "apps"];
+      for (var i = 0; i < dstargets.length; i++) {
+        if (args[dstargets[i]] === true) {
+          console.log("wiping " + dstargets[i]);
+          wipeDeviceStorageTarget(dstargets[i]);
+        }
+      }
+
+      if (args.reset === true) {
+        if (navigator.mozPower && navigator.mozPower.factoryReset) {
+          navigator.mozPower.factoryReset();
+        } else {
+          // FIXME can this really happen?
+          console.error("Something's wrong, mozPower is not available!");
+        }
       }
     }
   }
